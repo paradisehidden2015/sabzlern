1d45f249c92a4159cd7c963b71f453b9ac5b8570
paradise <paradisehidden2015@gmail.com>
2023-01-31 22:04:55 +0330

    CourseBox

--- frontend/src/Components/LastCourses/LastCourses.js
+++ frontend/src/Components/LastCourses/LastCourses.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./LastCourses.css";
 import SectionHeader from "../SectionHeader/SectionHeader";
+import CourseBox from "../CourseBox/CourseBox";
 
 export default function LastCourses() {
   return (
@@ -11,6 +12,18 @@ export default function LastCourses() {
           desc="سکوی پرتاپ شما به سمت موفقیت"
           btnTitle="تمامی دوره ها"
         />
+        <div class="courses-content">
+          <div class="container">
+            <div class="row">
+              <CourseBox />
+              <CourseBox />
+              <CourseBox />
+              <CourseBox />
+              <CourseBox />
+              <CourseBox />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
