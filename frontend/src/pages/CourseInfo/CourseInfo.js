5206c44cb1ce4087289f6e213dcb39e0ed268df2
paradise <paradisehidden2015@gmail.com>
2023-02-02 22:11:50 +0330

    Breadcrumb

--- frontend/src/pages/CourseInfo/CourseInfo.js
+++ frontend/src/pages/CourseInfo/CourseInfo.js
@@ -1,9 +1,31 @@
-import React from 'react'
-
-import './CourseInfo.css'
+import React from "react";
+import Topbar from "./../../Components/Topbar/Topbar";
+import Navbar from "./../../Components/Navbar/Navbar";
+import Footer from "./../../Components/Footer/Footer";
+import "./CourseInfo.css";
+import Breadcrumb from "../../Components/Breadcrumb/Breadcrumb";
 
 export default function CourseInfo() {
   return (
-    <div>CourseInfo</div>
-  )
+    <>
+      <Topbar />
+      <Navbar />
+      <Breadcrumb
+        links={[
+          { id: 1, title: "خانه", to: "/" },
+          {
+            id: 2,
+            title: "آموزش برنامه نویسی فرانت اند",
+            to: "/Category-info/frontend",
+          },
+          {
+            id: 3,
+            title: "دوره متخصص جاوا اسکریپت",
+            to: "/Course-info/js-expert",
+          },
+        ]}
+      />
+      <Footer />
+    </>
+  );
 }
