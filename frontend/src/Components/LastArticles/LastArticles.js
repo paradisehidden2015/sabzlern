2808ec9475aff6dcf2112aadc02ebf25c0e8a177
paradise <paradisehidden2015@gmail.com>
2023-02-02 12:11:15 +0330

    ArticleBox

--- frontend/src/Components/LastArticles/LastArticles.js
+++ frontend/src/Components/LastArticles/LastArticles.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ArticleBox from "../ArticleBox/ArticleBox";
 import SectionHeader from "../SectionHeader/SectionHeader";
 import "./LastArticles.css";
 export default function LastArticles() {
@@ -10,6 +11,28 @@ export default function LastArticles() {
           desc="پیش به سوی ارتقای دانش"
           btnTitle="تمامی مقاله ها"
         />
+        <div class="articles__content">
+          <div class="row">
+            <ArticleBox
+              cover="images/blog/3.jpg"
+              title="نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون"
+              desc="زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه
+            های مختلفی برای تسریع..."
+            />
+            <ArticleBox
+              cover="images/blog/3.jpg"
+              title="نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون"
+              desc="زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه
+            های مختلفی برای تسریع..."
+            />
+            <ArticleBox
+              cover="images/blog/3.jpg"
+              title="نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون"
+              desc="زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه
+            های مختلفی برای تسریع..."
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
