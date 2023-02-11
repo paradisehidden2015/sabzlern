1adde837b50ad7c513d5314cbba3c65dd7ff894e
paradise <paradisehidden2015@gmail.com>
2023-02-11 13:26:08 +0330

    AuthContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRoutes } from "react-router-dom";
 import routes from "./routes";
+import AuthContext from "./context/authContext";
 import "./App.css";
 
 export default function App() {
+  const [isLogIn, setIsLogIn] = useState(false);
+  const [token, setToken] = useState(false);
+  const [userInfos, setUserInfos] = useState({});
+
   const router = useRoutes(routes);
-  return <div>{router}</div>;
+
+  const login = (token) => {
+    setToken(token);
+    localStorage.setItem("user", JSON.stringify({ token }));
+  };
+  const logout = () => {
+    setToken(null);
+    setUserInfos({});
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLogIn,
+        token,
+        userInfos,
+        login,
+        logout,
+      }}
+    >
+      {router}
+    </AuthContext.Provider>
+  );
 }
---
+++ frontend/src/context/authContext.js
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+const AuthContext = createContext({
+  isLogIn: false,
+  token: null,
+  userInfos: null,
+  login: () => {},
+  logout: () => {},
+});
+export default AuthContext;
