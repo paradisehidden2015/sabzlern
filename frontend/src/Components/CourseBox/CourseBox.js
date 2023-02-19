988350aef500beddb9dc1200b51eeb45fc637599
paradise <paradisehidden2015@gmail.com>
2023-02-19 20:31:37 +0330

    CourseBox-props

--- frontend/src/Components/CourseBox/CourseBox.js
+++ frontend/src/Components/CourseBox/CourseBox.js
@@ -1,40 +1,39 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import CircleSpinner from "../CircleSpinner/CircleSpinner";
 
 import "./CourseBox.css";
 
-export default function CourseBox() {
+export default function CourseBox(props) {
+  const [isImgShow, setIsImgShow] = useState(false);
 
-  const [isImgShow, setIsImgShow] = useState(false)
-
-  const onImageLoaded = () => setIsImgShow(true)
+  const onImageLoaded = () => setIsImgShow(true);
 
   const onImageError = () => {
     // Codes
-  }
+  };
 
   return (
     <div className="col-4">
       <div className="course-box">
-        <a href="#">
+        <Link to={`/course-info/${props.shortName}`}>
           <img
-            src="/images/courses/fareelancer.png"
+            src={props.cover}
             // src="https://placeimg.com/295/295/any/tech?t=190129384"
             alt="Course img"
             className="course-box__img"
             onLoad={onImageLoaded}
             onError={onImageError}
           />
-          {
-            !isImgShow && (
-              <CircleSpinner />
-            )
-          }
-        </a>
+          {!isImgShow && <CircleSpinner />}
+        </Link>
         <div className="course-box__main">
-          <a href="#" className="course-box__title">
-            دوره پروژه محور متخصص جنگو
-          </a>
+          <Link
+            to={`/course-info/${props.shortName}`}
+            className="course-box__title"
+          >
+            {props.name}
+          </Link>
 
           <div className="course-box__rating-teacher">
             <div className="course-box__teacher">
@@ -77,15 +76,20 @@ export default function CourseBox() {
               <i className="fas fa-users course-box__users-icon"></i>
               <span className="course-box__users-text">500</span>
             </div>
-            <span className="course-box__price">1,000,000</span>
+            <span className="course-box__price">
+              {/* {props.price === 0 ? "رایگان" : props.price.toLocaleString()} */}
+            </span>
           </div>
         </div>
 
         <div className="course-box__footer">
-          <a href="#" className="course-box__footer-link">
+          <Link
+            to={`/course-info/${props.shortName}`}
+            className="course-box__footer-link"
+          >
             مشاهده اطلاعات
             <i className="fas fa-arrow-left course-box__footer-icon"></i>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
